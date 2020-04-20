7133b8145bab13dc0072e9dea83df039f91ba96e
ramon_ramirez <marluxia122@hotmail.com>
2020-04-20 00:32:20 -0600

    wrrongtype

--- api/models/dog.js
+++ api/models/dog.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const dogSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
-    name: { type: Text , require: true },
-    dogImage:{ type: Text , require: true },
-    status:{ type: Text , require: true },
-    platenumber:{ type: Text , require: true }
+    name: { type: String , require: true },
+    dogImage:{ type: String , require: true },
+    status:{ type: String , require: true },
+    platenumber:{ type: String , require: true }
 });
 
 module.exports= mongoose.model('Dog', dogSchema);
