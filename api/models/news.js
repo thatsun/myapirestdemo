e239c81284990c3116747c88d89301e4d5ccea06
ramon_ramirez <marluxia122@hotmail.com>
2020-04-22 21:37:17 -0600

    really

---
+++ api/models/news.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const newpostSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    postheader:{ type: String ,required:true },
+    postmessage:{ type: String ,required:true },
+    postedby:{ type: String ,required:true },
+    postdog:{ type: String ,required:true },
+    postdogid: { type: mongoose.Schema.Types.ObjectId, ref: 'Dog', require: true},
+    postmode:{ type: String ,required:true },
+    postplate:{ type: String ,required:true },
+    postuserid:{ type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true}
+});
+
+module.exports= mongoose.model('Newspost', newpostSchema);
